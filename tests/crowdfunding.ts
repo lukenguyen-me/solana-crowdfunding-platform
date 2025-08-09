9962ec660d61e5977db09ade66c53e5a4237a9df
Loc Nguyen <phuclo>
2025-08-09 18:49:07 +0700

    feat: add unit test for donation instruction

--- tests/crowdfunding.ts
+++ tests/crowdfunding.ts
@@ -72,8 +72,6 @@ describe("crowdfunding", () => {
     const campaignDescription = "Description of campaign A";
     const campaignTargetAmount = anchor.web3.LAMPORTS_PER_SOL * 5;
 
-    const startTime = getUnixTimestamp();
-
     await program.methods
       .createCampaign(
         campaignName,
@@ -92,10 +90,77 @@ describe("crowdfunding", () => {
       campaign.publicKey
     );
 
-    assert.equal(campaignAccount.name, campaignName);
-    assert.equal(campaignAccount.description, campaignDescription);
-    assert.equal(campaignAccount.targetAmount.toNumber(), campaignTargetAmount);
-    assert.equal(campaignAccount.amountPledged.toNumber(), 0);
-    assert.deepEqual(campaignAccount.status, { active: {} });
+    assert.equal(
+      campaignAccount.name,
+      campaignName,
+      "Campaign name should be equal to campaign name"
+    );
+    assert.equal(
+      campaignAccount.description,
+      campaignDescription,
+      "Campaign description should be equal to campaign description"
+    );
+    assert.equal(
+      campaignAccount.targetAmount.toNumber(),
+      campaignTargetAmount,
+      "Campaign target amount should be equal to campaign target amount"
+    );
+    assert.equal(
+      campaignAccount.amountPledged.toNumber(),
+      0,
+      "Campaign amount pledged should be equal to 0"
+    );
+    assert.deepEqual(
+      campaignAccount.status,
+      { active: {} },
+      "Campaign status should be active"
+    );
+  });
+
+  it("Allow a user to donate to a campaign", async () => {
+    const donateAmount = anchor.web3.LAMPORTS_PER_SOL * 1;
+
+    const campaignBalanceBefore = await provider.connection.getBalance(
+      campaign.publicKey
+    );
+    const donatorBalanceBefore = await provider.connection.getBalance(
+      donator.publicKey
+    );
+
+    await program.methods
+      .donate(new anchor.BN(donateAmount))
+      .accounts({
+        campaign: campaign.publicKey,
+        donator: donator.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([donator])
+      .rpc();
+
+    const campaignAccount = await program.account.campaign.fetch(
+      campaign.publicKey
+    );
+    const campaignBalanceAfter = await provider.connection.getBalance(
+      campaign.publicKey
+    );
+    const donatorBalanceAfter = await provider.connection.getBalance(
+      donator.publicKey
+    );
+
+    assert.equal(
+      campaignAccount.amountPledged.toNumber(),
+      donateAmount,
+      "Amount pledged should be equal to donate amount"
+    );
+    assert.isAtLeast(
+      campaignBalanceAfter,
+      campaignBalanceBefore + donateAmount,
+      "Campaign balance should be at least campaign balance before + donate amount"
+    );
+    assert.isAtMost(
+      donatorBalanceAfter,
+      donatorBalanceBefore - donateAmount,
+      "Donator balance should be below donator balance before - donate amount"
+    );
   });
 });
